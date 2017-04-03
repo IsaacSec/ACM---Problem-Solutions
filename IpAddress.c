1a39fcfe1d7db85a6667f7019da2b30c037d9eac
IsaacSec <isaacsecundino3@gmail.com>
2017-04-03 11:40:54 -0500

    Add files via upload

---
+++ IpAddress.c
@@ -0,0 +1,35 @@
+/* Ip Address */
+#include <stdio.h>
+#include <stdlib.h>
+
+int pow2[]={1,2,4,8,16,32,64,128};
+
+void printAddress(char *string){
+  int i;
+  for (i=0; i<4; i++) {
+    int octet = 0;
+    int j;
+    for (j=0; j<8; j++) {
+      int value = (int)(string[i*8+j])-48;
+      value = value*pow2[7-j];
+      octet += value;
+    }
+    if(i==3){
+      printf("%d\n",octet);
+    }else{
+      printf("%d.",octet);
+    }
+  }
+}
+
+int main(){
+  int n;
+  scanf("%d\n",&n);
+  int i;
+  for (i=0; i<n; i++) {
+    char *bitAdd = (char*)malloc(33);
+    scanf("%s\n",bitAdd);
+    printAddress(bitAdd);
+  }
+  return (0);
+}
