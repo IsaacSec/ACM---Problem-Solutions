a18045e37e11af614c610325e7ea2c58816ab951
IsaacSec <isaacsecundino3@gmail.com>
2017-04-07 00:09:16 -0500

    Little change to test octophus with atom

--- Gran Premio Mexico 2017 - Clasificatoria 1/sequence.cpp
+++ Gran Premio Mexico 2017 - Clasificatoria 1/sequence.cpp
@@ -3,19 +3,20 @@
 #include <math.h>
 
 
-int main(){
+int main()
+{
     int cases;
-
     scanf("%d\n",&cases);
+
     for(int i=0;i<cases;i++){
         double total = 0;
         int s;
         scanf("%d\n",&s);
         int j=0;
+
         while(total<s){
             j++;
             total+= 1/(sqrt((double)j)+sqrt((double)j+1));
-            //printf("%f\n",total);
         }
         printf("%d\n",j);
     }
